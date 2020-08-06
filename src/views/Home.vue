a5b115e8c1bf46767b6e92f6007250d6eaff61f6
MY <runay@golive.cc>
2020-08-06 15:10:42 +0800

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
     <section class="home">
         <van-button type="primary">主要按钮</van-button>
-        vue 移动端
+        <div class="container"></div>
+        <div>
+            {{ date }}
+        </div>
     </section>
 </template>
 
@@ -11,14 +14,54 @@ import { util } from '@/utils';
 
 @Component
 export default class Home extends Vue {
+    date = '';
     created() {
-        console.log(util.format('yyyy-MM-dd'));
+        setInterval(() => {
+            this.date = util.format('yyyy-MM-dd hh:mm:ss');
+        }, 1000);
     }
 }
 </script>
 
 <style lang="less" scoped>
-img {
-    width: 200px;
+.container {
+    position: relative;
+    width: 500px;
+    height: 250px;
+    background: #fff;
+    border-radius: 100%;
+    margin: 100px auto;
+    border: 6px solid #000;
+    border-bottom-width: 250px;
+    animation: animat 0.3s linear infinite;
+    &::before,
+    &::after {
+        content: '';
+        position: absolute;
+        width: 70px;
+        height: 70px;
+        border-radius: 100%;
+        top: 50%;
+        border: 90px solid;
+    }
+    &::before {
+        background: #fff;
+        left: 0;
+        border-color: #000;
+    }
+    &::after {
+        background: #000;
+        right: 0;
+        border-color: #fff;
+    }
+}
+
+@keyframes animat {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
 }
 </style>
